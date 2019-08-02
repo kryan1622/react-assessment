0f0876633698bc58a1ca8e87c8508e371d947171
kryan1622 <krystalryan95@hotmail.co.uk>
2019-08-02 10:22:11 +0100

    navbar added

---
+++ src/Components/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav } from 'reactstrap';
+  import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+import Login from './login.js';
+import Find from './find.js';
+import Create from './register.js';
+
+export default class Example extends React.Component {
+
+     render() {
+    return (
+      <div>
+          {/* <Router> */}
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/"> Reciepe Box</NavbarBrand>
+             <Nav className="ml-auto" navbar>
+                 {/* <Link to="/find">Find Users</Link>
+                 <Link to="/register">Register</Link>
+                 <Link to="/">Login</Link> */}
+
+                 </Nav>
+        </Navbar>
+      
+       {/* <Route exact path="/" component={Login} />
+        <Route  exact path="/register" component={Create} />
+        <Route path="/find" component={Find} />
+
+     
+        </Router> */}
+    
+      </div>
+    );
+  }
+}
---
+++ src/Components/register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Input, Form, Label, FormGroup, FormText } from 'reactstrap';
+
+export default class Create extends Component {
+
+    render() {
+
+        return (
+            console.log("hello")
+        )
+    }
+}
+
+ 
